09bb38592c0b889d67a5598b785892a711efe755
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-04-18 10:56:53 -0300

    Lesson 8. Data Structures

---
+++ src/com/java21days/CodeKeeper2.java
@@ -0,0 +1,34 @@
+package com.java21days;
+
+import java.util.ArrayList;
+
+public class CodeKeeper2 {
+    ArrayList<String> list;
+    String[] codes = { "alpha", "lambda", "gamma", "delta", "zeta" };
+
+    public CodeKeeper2(String[] userCodes) {
+        list = new ArrayList<>();
+
+        for (int i =0; i < codes.length; i++) {
+            addCode(codes[i]);
+        }
+
+        for (int j = 0; j < userCodes.length; j++) {
+            addCode(userCodes[j]);
+        }
+
+        for (String code : list) {
+            System.out.println(code);
+        }
+
+    }
+
+    private void addCode(String code) {
+        if (!list.contains(code))
+            list.add(code);
+    }
+
+    public static void main(String[] args) {
+        CodeKeeper2 keeper = new CodeKeeper2(args);
+    }
+}
